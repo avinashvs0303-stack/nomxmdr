607418651dce24b48a9767b4df32a5d6d034c408
avinashvs0303-stack <avinashvs0303@gmail.com>
2026-01-28 12:12:22 +0100

    Import index.css for styling
    Added import statement for index.css to apply styles.

--- index.tsx
+++ index.tsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'  // Add this at the top
+// ... rest of your code
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
